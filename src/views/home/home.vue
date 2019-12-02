d829cf24356fae1b13cc9098150564150e730d65
JimmyLLLL <851664281@qq.com>
2019-12-02 22:12:41 +0800

    initlize config

---
+++ src/views/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="homeWrapper">
+        <div class="home">helloworld</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'home'
+}
+</script>
+
+
+<style lang="scss" scoped>
+.homeWrapper{
+    .home{
+        color:yellow
+    }    
+}
+
+</style>
